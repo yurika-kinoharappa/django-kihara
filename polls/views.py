dfc6562740116c6faaafc6252deebb4788e7c0cf
kihara <yuyuririka64502@gmail.com>
2024-10-17 12:16:45 +0900

    wakarann

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,7 @@
-from django.db.models import F
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
 
-from .models import Choice, Question
+from .models import Question
 
 
 class IndexView(generic.ListView):
