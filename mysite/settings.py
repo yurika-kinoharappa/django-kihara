93d45420e335eede5ce8b46c4bc3f14eee1a2b2d
kihara <yuyuririka64502@gmail.com>
2024-10-30 12:04:09 +0900

    #9

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
+    "event.apps.EventConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+    path("event/", include("event.urls")),
     path("admin/", admin.site.urls),
 ]
