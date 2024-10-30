cc9df3657abee830e19947ba184634d5af557dd4
kihara <yuyuririka64502@gmail.com>
2024-10-30 18:13:32 +0900

    #10

--- event/models.py
+++ event/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Event(models.Model):
+class EventConfig(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
