3e9112952da1e004df01cdf2737b8f221b6e3a41
kihara <yuyuririka64502@gmail.com>
2024-11-06 12:22:29 +0900

    tutorial2 made

--- event/models.py
+++ event/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class EventConfig(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- event/views.py
+++ event/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from .models import EventConfig
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = EventConfig.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "event/index.html", context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def create(request):
+    # 画面で入力した名前をとってくる
+    # name = cxxxxx
+    e = EventConfig(name="party")
+    e.save()
+    return HttpResponse("You're voting on question")
