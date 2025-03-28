8739fcc9fdff46139b9153efa89c959755acccfb
kihara <yuyuririka64502@gmail.com>
2025-03-28 17:23:15 +0900

    a

--- polls/models.py
+++ polls/models.py
@@ -11,10 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-
-def was_published_recently(self):
-    now = timezone.now()
-    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
